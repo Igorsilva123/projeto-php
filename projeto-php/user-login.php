b5696047023a38a9737d6f80321f0407ff6b3f0a
Igorsilva123 <igor.silvabrito@outlook.com>
2025-11-27 18:53:35 -0300

    login cookies

--- projeto-php/user-login.php
+++ projeto-php/user-login.php
@@ -1,15 +1,20 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Origin: http://localhost:4200");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
 
 include 'config.inc.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-$email = $data['email'];
-$password = $data['password'];
+$email = $data['email'] ?? '';
+$password = $data['password'] ?? '';
 
 $sql = mysqli_prepare($conexao, "SELECT id, email, password FROM users WHERE email = ?");
 mysqli_stmt_bind_param($sql, "s", $email);
@@ -27,9 +32,25 @@ if (!password_verify($password, $user['password'])) {
     echo json_encode(["error" => "Senha incorreta"]);
     exit;
 }
-    echo json_encode([
-        "message" => "Login realizado com sucesso!"])
-        
 
+$token = bin2hex(random_bytes(32));
 
+$sqlToken = mysqli_prepare($conexao, "UPDATE users SET token = ? WHERE id = ?");
+mysqli_stmt_bind_param($sqlToken, "si", $token, $user['id']);
+mysqli_stmt_execute($sqlToken);
+
+setcookie(
+    "auth_token",    
+    $token,             
+    time() + (60*60*24), 
+    "/",               
+    "",                 
+    false,              
+    true             
+);
+
+echo json_encode([
+    "message" => "Login realizado com sucesso!",
+    "user_id" => $user['id']
+]);
 ?>
